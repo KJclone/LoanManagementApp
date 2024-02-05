c63379faf6f95d83607ae648f04bfd20a18d3b3f
KJclone <158156169+KJclone@users.noreply.github.com>
2024-02-05 22:59:42 +0530

    Commit 2 with SESSION_COOKIE_NAME

--- LoanManagement/settings.py
+++ LoanManagement/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import random,string
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,6 +130,9 @@ STATIC_ROOT = 'staticfiles'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'  
+SESSION_COOKIE_NAME = ''.join(random.choices(string.ascii_uppercase +
+                             string.digits, k=9))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
